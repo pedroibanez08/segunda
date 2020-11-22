5e6e846b5d0b4736019cf460ca5b2f8ff5157ef1
pedroibanez08 <60982891+pedroibanez08@users.noreply.github.com>
2020-11-22 19:29:34 +0100

    Add files via upload

---
+++ abrir_archivo_url.js
@@ -0,0 +1,22 @@
+const http=require('http');
+const fs=require('fs');
+const url=require('url');
+http.createServer(function(req,res){
+var q=url.parse(req.url,true);
+var filename="."+q.pathname;
+//console.log(filename);
+
+fs.readFile(filename,function(err,data){
+if(err){
+	res.writeHead(404,{'Content-type':'text/html;charset=utf-8'});
+	return res.end('404 not found');
+}
+res.writeHead(200,{'Content-type':'text/html;charset=utf-8'});
+res.write(data);
+console.log(res.end());
+return res.end();
+
+});
+
+
+}).listen(8080);
